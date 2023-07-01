a03d54e1759a57d7e9326c1b3508a991416da7b4
narendrajava1 <narendrakolli08@gmail.com>
2023-07-01 22:05:31 +0530

    added logback

--- src/main/java/com/naren/ApiGatewayApplication.java
+++ src/main/java/com/naren/ApiGatewayApplication.java
@@ -1,14 +1,19 @@
 package com.naren;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import reactor.core.publisher.Hooks;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
-
 	public static void main(String[] args) {
+		Hooks.enableContextLossTracking();
+		
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
